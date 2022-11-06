cf71e3be1a443b72295ee8948d3c189186213f10
Dodul01 <105786357+Dodul01@users.noreply.github.com>
2022-11-06 13:07:16 +0600

    2 files modified

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,18 +1,24 @@
 import React,{useState,useEffect} from 'react';
-
-// API => https://jsonplaceholder.typicode.com/posts
+import './style.css';
 
 const Post = () =>{
   const [post, setPost] = useState([]);
   useEffect(()=>{
     const fetchPost = async ()=>{
-      
+      let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      let data = await response.json();
+      setPost(data);
     }
+    fetchPost();
   },[])
+  
  return(
     <div>
       <h1>Post</h1>
-
+      {post.map((post)=> <div key={post.id} className="post-card">
+        <h4 className="title">{post.title}</h4>
+        <p>{post.body}</p>
+      </div>)}
     </div>
   )
 }
